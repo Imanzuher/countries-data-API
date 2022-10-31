5a22efca380c78053aa74488eb7e924f0152ba10
Imanzuher <imoojaf@gmail.com>
2022-10-31 16:33:00 +0300

    tailwind css setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 function App() {
-  return <div className="App">helllo</div>;
+  return (
+    <div className="App">
+      <h1 className="text-red-500 text-4xl">iman</h1>
+    </div>
+  );
 }
 
 export default App;
